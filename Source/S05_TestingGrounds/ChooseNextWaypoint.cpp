e95fbc80c70095c76d454d0cac3e53614bb7274e
DaveQc <asgaridavid@gmail.com>
2017-09-13 16:07:23 -0400

    0.0.11

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,12 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
 
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++"));
+	//Get the patrol points
+
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// set next waypoint
+
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	BlackboardComp->SetValueAsObject(WayPointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// TODO Protect against empty patrol routes
+
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint index : %i"), Index);
+
 	return EBTNodeResult::Succeeded;
 
+
+	
 }
 
 
